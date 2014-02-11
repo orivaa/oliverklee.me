3797d1d4badd2c2d6fcaa2f71eb73a31d75ea42f
Oliver Klee <oliverklee@Olivers-MacBook-Pro.local>
2014-02-12 00:05:37 +0100

    Resubmit
    Did a new Setup with Yeoman

---
+++ app/scripts/main.js
@@ -0,0 +1,61 @@
+window.mySwipe = new Swipe(document.getElementById('slider'), {
+  startSlide: 1,
+  speed: 400,
+  auto: 3000,
+  continuous: true,
+  disableScroll: false,
+  callback: function(index, elem) {},
+  transitionEnd: function(index, elem) {}
+});
+
+jQuery(function($) {
+  // This will hold the state of the pop-up menu
+  var open = false;
+
+  function resizeMenu() {
+      // If window is less than 600px wide
+      if ($(this).width() < 600) {
+          if (!open) {
+              // Hide the menu if it's not supposed to be displayed
+              $("#mainnav").hide();
+          }
+          // Display the button
+          $(".show-menu").show();
+      }
+      else if ($(this).width() >= 600) {
+          // If window is wider than 600px
+          if (!open) {
+              // Show the menu if it's not displayed yet
+              $("#mainnav").show();
+          }
+          // Hide the button if the screen is wide enough for the menu to always show
+          $(".show-menu").hide();
+      }
+  }
+
+  function setupMenuButton() {
+      $(".show-menu").click(function(e) {
+          e.preventDefault();
+
+          // If already shown...
+          if (open) {
+              // Hide the menu
+              $("#mainnav").slideToggle().fadeOut();
+              $(".show-menu").toggleClass("selected");
+          }
+          else {
+              // If not shown, show the menu
+              $("#mainnav").slideToggle().fadeIn();
+              $(".show-menu").toggleClass("selected");
+          }
+          open = !open;
+      });
+  }
+   
+  // Add a handler function for the resize event of window
+  $(window).resize(resizeMenu);
+
+  // Initialize the menu and the button
+  resizeMenu();
+  setupMenuButton();
+});
---
+++ app/scripts/plugins.js
@@ -0,0 +1,38 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+// Place any jQuery/helper plugins in here.
+(function($){
+$('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - 20;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+})(jQuery);
