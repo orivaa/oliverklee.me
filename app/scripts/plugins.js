083e2096de67e288d4adbbc71d5f58f5dc96716e
Oliver Klee <oliverklee@Olivers-MacBook-Pro.local>
2014-02-18 22:42:51 +0100

    Code formatting

--- app/scripts/plugins.js
+++ app/scripts/plugins.js
@@ -23,16 +23,16 @@
 
 // Place any jQuery/helper plugins in here.
 (function($){
-$('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-        && location.hostname == this.hostname) {
-            var $target = $(this.hash);
-            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-            if ($target.length) {
-                var targetOffset = $target.offset().top - 20;
-                $('html,body').animate({scrollTop: targetOffset}, 1000);
-                return false;
-            }
+    $('a[href*=#]').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+            && location.hostname == this.hostname) {
+                var $target = $(this.hash);
+                $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+                if ($target.length) {
+                    var targetOffset = $target.offset().top - 20;
+                    $('html,body').animate({scrollTop: targetOffset}, 1000);
+                    return false;
+                }
         }
     });
 })(jQuery);
