807edf25cfb64ef4eee3f7d3f00d3084b227801d
Oliver Klee <oliverklee@olivers-mbp.fritz.box>
2014-04-06 09:49:59 +0200

    Restructuring

--- app/scripts/plugins.js
+++ app/scripts/plugins.js
@@ -24,15 +24,15 @@
 // Place any jQuery/helper plugins in here.
 (function($){
     $('a[href*=#]').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-            && location.hostname == this.hostname) {
-                var $target = $(this.hash);
-                $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
-                if ($target.length) {
-                    var targetOffset = $target.offset().top - 20;
-                    $('html,body').animate({scrollTop: targetOffset}, 1000);
-                    return false;
-                }
+        if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') &&
+                location.hostname === this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top - 20;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
         }
     });
 })(jQuery);
