b53cf890c9a2cc1b685b6d22e752bb8981a608fb
Oliver Klee <oliverklee@olivers-mbp.fritz.box>
2014-04-06 09:49:45 +0200

    Restructuring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,70 +4,69 @@
  * @author Oliver Klee
  * @version 0.5.2
  */
+
 jQuery(function($) {
 
-  window.mySwipe = new Swipe(document.getElementById('slider'), {
-    startSlide: 1,
-    speed: 400,
-    auto: 3000,
-    continuous: true,
-    disableScroll: false,
-    callback: function(index, elem) {},
-    transitionEnd: function(index, elem) {}
-  });
+    window.mySwipe = new Swipe(document.getElementById('slider'), {
+        startSlide: 1,
+        speed: 400,
+        auto: 4000,
+        continuous: true,
+        disableScroll: false,
+    });
 
-  initResizeMenu();
+    var initResizeMenu = function() {
+        // This will hold the state of the pop-up menu
+        var open = false;
+        var $mainNav = $('#mainnav');
+        var $showMenu =  $('.show-menu');
+
+        function resizeMenu() {
+            // If window is less than 600px wide
+            if ( $(this).width() < 600 ) {
+                if (!open) {
+                    // Hide the menu if it's not supposed to be displayed
+                    $mainNav.hide();
+                }
+                // Display the button
+                $showMenu.show();
+            } else if ( $(this).width() >= 600 ) {
+                // If window is wider than 600px
+                if (!open) {
+                    // Show the menu if it's not displayed yet
+                    $mainNav.show();
+                }
+                // Hide the button if the screen is wide enough for the menu to always show
+                $showMenu.hide();
+            }
+        }
+
+        function setupMenuButton() {
+            $showMenu.click(function(e) {
+                e.preventDefault();
+
+                // If already shown...
+                if (open) {
+                    // Hide the menu
+                    $mainNav.slideToggle().fadeOut();
+                    $showMenu.toggleClass('selected');
+                } else {
+                    // If not shown, show the menu
+                    $mainNav.slideToggle().fadeIn();
+                    $showMenu.toggleClass('selected');
+                }
+                open = !open;
+            });
+        }
+         
+        // Add a handler function for the resize event of window
+        $(window).resize(resizeMenu);
+
+        // Initialize the menu and the button
+        resizeMenu();
+        setupMenuButton();
+    };
+
+    initResizeMenu();
 
 });
-
-var initResizeMenu = function() {
-    // This will hold the state of the pop-up menu
-    var open = false;
-    var $mainNav = $("#mainnav");
-    var $showMenu =  $(".show-menu");
-
-    function resizeMenu() {
-        // If window is less than 600px wide
-        if ( $(this).width() < 600 ) {
-            if (!open) {
-                // Hide the menu if it's not supposed to be displayed
-                $mainNav.hide();
-            }
-            // Display the button
-            $showMenu.show();
-        } else if ( $(this).width() >= 600 ) {
-            // If window is wider than 600px
-            if (!open) {
-                // Show the menu if it's not displayed yet
-                $mainNav.show();
-            }
-            // Hide the button if the screen is wide enough for the menu to always show
-            $showMenu.hide();
-        }
-    }
-
-    function setupMenuButton() {
-        $showMenu.click(function(e) {
-            e.preventDefault();
-
-            // If already shown...
-            if (open) {
-                // Hide the menu
-                $mainNav.slideToggle().fadeOut();
-                $showMenu.toggleClass("selected");
-            } else {
-                // If not shown, show the menu
-                $mainNav.slideToggle().fadeIn();
-                $showMenu.toggleClass("selected");
-            }
-            open = !open;
-        });
-    }
-     
-    // Add a handler function for the resize event of window
-    $(window).resize(resizeMenu);
-
-    // Initialize the menu and the button
-    resizeMenu();
-    setupMenuButton();
-}
